f3beabb46ff66bfb2591da19adb3dcc868d624c8
lalyos <lalyos@yahoo.com>
2013-10-28 11:51:42 +0100

    Menu with OneToMany relation added

---
+++ src/main/java/com/eggs/jpa/Menu.java
@@ -0,0 +1,51 @@
+package com.eggs.jpa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+@Entity
+public class Menu {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    
+    private String name;
+
+    @OneToMany
+    private List<Food> foods = new ArrayList<Food>();
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return "Menu [id=" + id + ", name=" + name + "]";
+    }
+
+    public List<Food> getFoods() {
+        return foods;
+    }
+
+    public void setFoods(List<Food> foods) {
+        this.foods = foods;
+    }
+}
